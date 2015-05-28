3cc6126cd8e7671fb0e6e70b6bd40eb6af32c700
y.belyaeva <bieliaievays@gmail.com>
2015-05-28 17:54:09 +0300

    Add parsing

--- ENumbersData/src/main/java/CreateXmlFromHtml.java
+++ ENumbersData/src/main/java/CreateXmlFromHtml.java
@@ -1,9 +1,15 @@
 import org.apache.commons.io.FileUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.xembly.Directives;
 import org.xembly.ImpossibleModificationException;
 import org.xembly.Xembler;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by y.belyaeva on 27.05.2015.
@@ -15,8 +21,27 @@ public class CreateXmlFromHtml {
 
     public static void main(String[] args) {
 
+        ArrayList<ENumber> list = getENumbersFromHtml("http://en.wikipedia.org/wiki/E_number#E400.E2.80.93E499_.28thickeners.2C_stabilizers.2C_emulsifiers.29");
         CreateXML();
         //TODO create file with base struct and fill it
+    }
+
+    private static ArrayList<ENumber> getENumbersFromHtml(String URL) {
+        Document doc = null;
+        try {
+            doc = Jsoup.connect(URL).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Elements tables = doc.getElementsByTag("table"); //all tables
+        for (Element table : tables) {
+            Elements eNumbers = table.select("tr:matches(E[0-9]+"); //<tr> tags, full text of which matches regex
+            for (Element eNumber : eNumbers) {
+                String s = eNumber.text();
+                System.out.println(s);
+            }
+        }
+        return new ArrayList<ENumber>();
     }
 
     private static void CreateXML() {
---
+++ ENumbersData/src/main/java/ENumber.java
@@ -0,0 +1,11 @@
+/**
+ * Created by y.belyaeva on 28.05.2015.
+ */
+public class ENumber {
+    //TODO add get-sets?
+    private String code;
+    private String name;
+    private String purpose;
+    private String status;
+
+}
