31623f2ba143e0bf598f798af0d1102433c58e3d
y.belyaeva <bieliaievays@gmail.com>
2015-07-06 17:23:27 +0300

    XML parsing implemented

--- src/com/example/ENumbers/ENumber.java
+++ src/com/example/ENumbers/ENumber.java
@@ -8,27 +8,14 @@ import org.simpleframework.xml.Root;
  */
 @Root(name = "eNumber")
 public class ENumber {
-    @Element(name = "code")
-    private String _code = "";
-    @Element(name = "name")
-    private String _name = "";
-    @Element(name = "purpose")
-    private String _purpose = "";
-    @Element(name = "status")
-    private String _status = "";
-
-    public ENumber(String code, String name, String purpose, String status){
-        _code = code;
-        _name = name;
-        _purpose = purpose;
-        _status = status;
-    }
-
-    @Override
-    public String toString()
-    {
-        return "eNumber{" + "code=" + _code + ", name=" + _name + ", purpose=" + _purpose + ", status=" + _status +'}';
-    }
+    @Element(name = "code", required=false)
+    private String _code;
+    @Element(name = "name", required=false)
+    private String _name;
+    @Element(name = "purpose", required=false)
+    private String _purpose;
+    @Element(name = "status", required=false)
+    private String _status;
 
     public String get_code() {
         return _code;
