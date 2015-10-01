2a730216a78306ff4501f6af3ff92a90677ddfd3
bieliaievays <bieliaievays@gmail.com>
2015-10-01 12:09:23 +0100

    Add send mail to developer

--- src/com/example/ENumbers/AboutActivity.java
+++ src/com/example/ENumbers/AboutActivity.java
@@ -1,8 +1,12 @@
 package com.example.eNumbers;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.*;
 import android.widget.TextView;
 
@@ -14,6 +18,7 @@ public class AboutActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private TextView mTextView_appName;
     private TextView mTextView_developer;
+    private TextView mTextView_email;
     private TextView mTextView_version;
 
 
@@ -33,25 +38,31 @@ public class AboutActivity extends AppCompatActivity {
         mTextView_developer = (TextView) findViewById(R.id.developer);
         mTextView_developer.setText(R.string.developer);
 
+        mTextView_email = (TextView) findViewById(R.id.email);
+        mTextView_email.setText(Html.fromHtml("<u>"+ getString(R.string.email) + "</u>"));
+        mTextView_email.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                callSendMeMail();
+            }
+        });
+       // mTextView_email.setMovementMethod(LinkMovementMethod.getInstance()); TODO use for sourses
+
         mTextView_version = (TextView) findViewById(R.id.version);
         mTextView_version.setText(R.string.version);
     }
 
+    private void callSendMeMail() {
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                "mailto", mTextView_email.getText().toString(), null));
 
-    //TODO click email
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item){
-//        switch (item.getItemId()){
-//            case R.id.sendEmail:
-//                Intent Email = new Intent(Intent.ACTION_SEND);
-//                Email.setType("text/email");
-//                Email.putExtra(Intent.EXTRA_EMAIL, new String[] { "admin@hotmail.com" });
-//                Email.putExtra(Intent.EXTRA_SUBJECT, "Feedback");
-//                Email.putExtra(Intent.EXTRA_TEXT, "Dear ...," + "");
-//                startActivity(Intent.createChooser(Email, "Send Feedback:"));
-//                return true;
-//        }
-//    }
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[] { mTextView_email.getText().toString() });
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Feedback");
+        startActivity(Intent.createChooser(emailIntent, "Send mail to Developer:"));
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
