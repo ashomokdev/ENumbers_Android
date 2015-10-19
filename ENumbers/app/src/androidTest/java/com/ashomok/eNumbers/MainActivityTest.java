8cf8d0f585d6ebf109430924e08acc61d54ffdf2
bieliaievays <bieliaievays@gmail.com>
2015-10-19 22:24:46 +0200

    Move to AndroidStudio from Idea and to Gradle from Maven

---
+++ ENumbers/app/src/androidTest/java/com/ashomok/eNumbers/MainActivityTest.java
@@ -0,0 +1,31 @@
+package com.ashomok.eNumbers;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.IsolatedContext;
+import android.test.mock.MockContentResolver;
+
+import junit.framework.Assert;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    public MainActivityTest() {
+
+        super("com.ashomok.eNumbers", MainActivity.class);
+    }
+
+    public void testSQLiteAssetHelper() {
+
+        IsolatedContext _openHelperContext = new IsolatedContext(new MockContentResolver(), getActivity());
+
+        ENumbersSQLiteAssetHelper helper = new ENumbersSQLiteAssetHelper(_openHelperContext);
+
+        Assert.assertTrue(helper.selectRowsByCodes(new String[]{"E100"}).getCount() == 1);
+
+        Assert.assertTrue(helper.selectRowsByCodes(new String[]{"E100", "E123"}).getCount() == 2);
+    }
+
+}
--- README.md
+++
@@ -1,5 +0,0 @@
-# ENumbers_Android_Java_Maven
-Simple Android app for getting info about E numbers (food components)
-
-base.xml contents all data for the app.
-For generate base.xml run ENumbers\ENumbersData\src\main\java\CreateXmlFromHtml.java
