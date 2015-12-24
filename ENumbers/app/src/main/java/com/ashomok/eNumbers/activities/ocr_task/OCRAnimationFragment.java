625744a6823e44c10bd1bc4b551148ade0d5d479
bieliaievays <bieliaievays@gmail.com>
2015-12-24 11:48:03 +0200

    Animation fully implemented

--- ENumbers/app/src/main/java/com/ashomok/eNumbers/activities/ocr_task/OCRAnimationFragment.java
+++ ENumbers/app/src/main/java/com/ashomok/eNumbers/activities/ocr_task/OCRAnimationFragment.java
@@ -7,10 +7,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.ColorMatrix;
-import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Matrix;
-import android.graphics.Paint;
 import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
@@ -20,8 +17,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-
 
 import com.ashomok.eNumbers.R;
 
@@ -37,7 +32,6 @@ import java.nio.channels.FileChannel;
  * Created by Iuliia on 18.12.2015.
  */
 public class OCRAnimationFragment extends Fragment {
-    //    private ImageView image;
     private static final int MAX_IMAGE_DIMENSION = 512;
 
     private Uri imageUri;
@@ -48,138 +42,31 @@ public class OCRAnimationFragment extends Fragment {
         super.onCreate(savedInstanceState);
 
         // retain this fragment
-        // setRetainInstance(true);
+        setRetainInstance(true);
 
         Bundle bundle = this.getArguments();
         if (bundle != null) {
             imageUri = bundle.getParcelable("image");
         }
-
-
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        // return inflater.inflate(R.layout.ocr_animation_fragment, container, false);
 
-        return new OCRAnimationView(getActivity());
-    }
-//
-//    @Override
-//    public void onViewCreated(View view, Bundle savedInstanceState) {
-//        super.onViewCreated(view, savedInstanceState);
-//
-//        image = (ImageView) view.findViewById(R.id.image);
-//        imageBitmap = prepareImage(imageUri);
-//        image.setImageBitmap(imageBitmap);
-//
-//    }
-
-    private Bitmap prepareImage(Uri imageUri) {
-        try {
-            return getCorrectlyOrientedImage(getActivity().getApplicationContext(), imageUri);
-        } catch (IOException e) {
-            e.printStackTrace();
-            Log.e(this.getClass().getCanonicalName(), e.getMessage());
-        }
-        return null;
-    }
-
-    public Bitmap getCorrectlyOrientedImage(Context context, Uri photoUri) throws IOException {
-        InputStream is = context.getContentResolver().openInputStream(photoUri);
-        BitmapFactory.Options dbo = new BitmapFactory.Options();
-        dbo.inJustDecodeBounds = true;
-        BitmapFactory.decodeStream(is, null, dbo);
-        is.close();
-
-        int rotatedWidth, rotatedHeight;
-
-        ExifInterface exif = new ExifInterface(photoUri.getEncodedPath());
-
-        int exifOrientation = exif.getAttributeInt(
-                ExifInterface.TAG_ORIENTATION,
-                ExifInterface.ORIENTATION_NORMAL);
-
-        if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_90 ||
-                exifOrientation == ExifInterface.ORIENTATION_ROTATE_270) {
-            rotatedWidth = dbo.outHeight;
-            rotatedHeight = dbo.outWidth;
-        } else {
-            rotatedWidth = dbo.outWidth;
-            rotatedHeight = dbo.outHeight;
-        }
-
-        Bitmap srcBitmap;
-        is = context.getContentResolver().openInputStream(photoUri);
-        if (rotatedWidth > MAX_IMAGE_DIMENSION || rotatedHeight > MAX_IMAGE_DIMENSION) {
-            float widthRatio = ((float) rotatedWidth) / ((float) MAX_IMAGE_DIMENSION);
-            float heightRatio = ((float) rotatedHeight) / ((float) MAX_IMAGE_DIMENSION);
-            float maxRatio = Math.max(widthRatio, heightRatio);
-
-            // Create the bitmap from file
-            BitmapFactory.Options options = new BitmapFactory.Options();
-            options.inSampleSize = (int) maxRatio;
-            srcBitmap = BitmapFactory.decodeStream(is, null, options);
-        } else {
-            srcBitmap = BitmapFactory.decodeStream(is);
-        }
-        is.close();
-
-    /*
-     * if the orientation is not 0 (or -1, which means we don't know), we
-     * have to do a rotation.
-     */
-
-        int rotate = 0;
-        switch (exifOrientation) {
-            case ExifInterface.ORIENTATION_ROTATE_90:
-                rotate = 90;
-                break;
-            case ExifInterface.ORIENTATION_ROTATE_180:
-                rotate = 180;
-                break;
-            case ExifInterface.ORIENTATION_ROTATE_270:
-                rotate = 270;
-                break;
-        }
-        if ((getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE &&
-                (rotatedWidth > rotatedHeight)) ||
-                (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) &&
-                        (rotatedWidth < rotatedHeight)) {
-            //fits ok
-
-        } else {
-            //shod be rotated in -90 degrees.
-            rotate -= 90;
-        }
-
-
-        if (rotate != 0) {
-            // Setting pre rotate
-            Matrix mtx = new Matrix();
-            mtx.postRotate(rotate);
-
-            // Rotating Bitmap
-            srcBitmap = Bitmap.createBitmap(srcBitmap, 0, 0, srcBitmap.getWidth(),
-                    srcBitmap.getHeight(), mtx, true);
-        }
-
-        return srcBitmap;
+        return new OCRAnimationView(getActivity(), imageUri);
     }
 
     private class OCRAnimationView extends View {
-//        private final float acc;
         private float dX;
-        Bitmap background;
-        Bitmap binarizated;
-        Bitmap scanband;
-        int screenW;
-        int screenH;
-        float X;
-        float Y;
+        private Bitmap background;
+        private Bitmap binarizated;
+        private Bitmap scanband;
+        private int screenW;
+        private int screenH;
+        private float X;
+        private float Y;
+        private int scanBandW = 50;
         /**
          * Boolean that tells me how to treat a transparent pixel (Should it be black?)
          */
@@ -190,15 +77,14 @@ public class OCRAnimationFragment extends Fragment {
          * then we should be this percent far from WHITE to be in the black region.
          * Example: If this value is 0.5, the space is equally split.
          */
-       // private static final double SPACE_BREAKING_POINT = 13.0 / 30.0;
-        private static final double SPACE_BREAKING_POINT = 0.72;
+        // private static final double SPACE_BREAKING_POINT = 13.0 / 30.0;
+        private static final double SPACE_BREAKING_POINT = 0.5;
 
-        public OCRAnimationView(Context context) {
+        public OCRAnimationView(Context context, Uri imageUri) {
             super(context);
 
             X = 0;
             Y = 0;
-//            acc = 0.07f; //acceleration
             dX = 0.7f; //horizontal speed
 
             Bitmap imageBitmap = prepareImage(imageUri);
@@ -207,11 +93,9 @@ public class OCRAnimationFragment extends Fragment {
             } else {
                 throw new NullPointerException("imageBitmap == null. Obtain imageBitmap before call.");
             }
-
             scanband = BitmapFactory.decodeResource(getResources(), R.drawable.scan_band); //load a scanband image
 
-            if (! background.isMutable())
-            {
+            if (!background.isMutable()) {
                 background = convertToMutable(background);
             }
             binarizated = createBinarizated(background);
@@ -223,31 +107,136 @@ public class OCRAnimationFragment extends Fragment {
             super.onSizeChanged(w, h, oldw, oldh);
             screenW = w;
             screenH = h;
+
+            //TODO forbid rotation
+
             background = Bitmap.createScaledBitmap(background, w, h, true); //Resize background to fit the screen.
+            binarizated = Bitmap.createScaledBitmap(binarizated, w, h, true); //Resize to fit the screen.
+
             scanband = Bitmap.createScaledBitmap(scanband, scanband.getWidth(), h, true);
+            if (X > screenW - scanBandW) {
+                X = screenW - scanBandW;
+            }
         }
 
         @Override
         public void onDraw(Canvas canvas) {
             super.onDraw(canvas);
 
-            //Draw background.
+            //Draw background
             canvas.drawBitmap(background, 0, 0, null);
 
-//            //Draw recognized part.
-//            canvas.drawBitmap(binarizated, 0, 0, null);
+            Bitmap croppedBitmap = Bitmap.createBitmap(binarizated, (int) X, 0, scanBandW, screenH);
+            canvas.drawBitmap(croppedBitmap, X, 0, null);
 
-           //Compute scanband speed and location.
-            int scanBandW = 0; //todo
-            X+= dX; //Increase or decrease horizontal position.
+            //Compute scanband speed and location.
+            X += dX; //Increase or decrease horizontal position.
             if ((X > (screenW - scanBandW)) || (X < 0)) {
-                dX=(-1f)*dX; //Reverse speed when one side hit.
+                dX = (-1f) * dX; //Reverse speed when one side hit.
             }
+
             //Draw scanband
-            canvas.drawBitmap(scanband, X, Y, null);
+            if (dX > 0) //moving from left to right
+            {
+                canvas.drawBitmap(scanband, X + scanBandW, Y, null);
+            } else {
+                canvas.drawBitmap(scanband, X, Y, null);
+            }
 
             //Call the next frame.
             invalidate();
+        }
+
+        private Bitmap prepareImage(Uri imageUri) {
+            try {
+                return getCorrectlyOrientedImage(getActivity().getApplicationContext(), imageUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.e(this.getClass().getCanonicalName(), e.getMessage());
+            }
+            return null;
+        }
+
+        private Bitmap getCorrectlyOrientedImage(Context context, Uri photoUri) throws IOException {
+            InputStream is = context.getContentResolver().openInputStream(photoUri);
+            BitmapFactory.Options dbo = new BitmapFactory.Options();
+            dbo.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(is, null, dbo);
+            is.close();
+
+            int rotatedWidth, rotatedHeight;
+
+            ExifInterface exif = new ExifInterface(photoUri.getEncodedPath());
+
+            int exifOrientation = exif.getAttributeInt(
+                    ExifInterface.TAG_ORIENTATION,
+                    ExifInterface.ORIENTATION_NORMAL);
+
+            if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_90 ||
+                    exifOrientation == ExifInterface.ORIENTATION_ROTATE_270) {
+                rotatedWidth = dbo.outHeight;
+                rotatedHeight = dbo.outWidth;
+            } else {
+                rotatedWidth = dbo.outWidth;
+                rotatedHeight = dbo.outHeight;
+            }
+
+            Bitmap srcBitmap;
+            is = context.getContentResolver().openInputStream(photoUri);
+            if (rotatedWidth > MAX_IMAGE_DIMENSION || rotatedHeight > MAX_IMAGE_DIMENSION) {
+                float widthRatio = ((float) rotatedWidth) / ((float) MAX_IMAGE_DIMENSION);
+                float heightRatio = ((float) rotatedHeight) / ((float) MAX_IMAGE_DIMENSION);
+                float maxRatio = Math.max(widthRatio, heightRatio);
+
+                // Create the bitmap from file
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = (int) maxRatio;
+                srcBitmap = BitmapFactory.decodeStream(is, null, options);
+            } else {
+                srcBitmap = BitmapFactory.decodeStream(is);
+            }
+            is.close();
+
+    /*
+     * if the orientation is not 0 (or -1, which means we don't know), we
+     * have to do a rotation.
+     */
+
+            int rotate = 0;
+            switch (exifOrientation) {
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    rotate = 90;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    rotate = 180;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    rotate = 270;
+                    break;
+            }
+            if ((getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE &&
+                    (rotatedWidth > rotatedHeight)) ||
+                    (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) &&
+                            (rotatedWidth < rotatedHeight)) {
+                //fits ok
+
+            } else {
+                //shod be rotated in -90 degrees.
+                rotate -= 90;
+            }
+
+
+            if (rotate != 0) {
+                // Setting pre rotate
+                Matrix mtx = new Matrix();
+                mtx.postRotate(rotate);
+
+                // Rotating Bitmap
+                srcBitmap = Bitmap.createBitmap(srcBitmap, 0, 0, srcBitmap.getWidth(),
+                        srcBitmap.getHeight(), mtx, true);
+            }
+
+            return srcBitmap;
         }
 
         private boolean shouldBeBlack(int pixel) {
@@ -268,7 +257,7 @@ public class OCRAnimationFragment extends Fragment {
         }
 
         private Bitmap createBinarizated(Bitmap source) {
-            int customColor = ContextCompat.getColor(getActivity(), R.color.blue);
+            int customColor = ContextCompat.getColor(getActivity(), R.color.light_blue);
             binarizated = Bitmap.createBitmap(source);
             // I will look at each pixel and use the function shouldBeBlack to decide
             // whether to make it black or otherwise white
@@ -279,9 +268,7 @@ public class OCRAnimationFragment extends Fragment {
                         try {
                             binarizated.setPixel(i, c, customColor);
                             //binarizated.setPixel(i, c, Color.BLACK);
-                        }
-                        catch (Exception e)
-                        {
+                        } catch (Exception e) {
                             e.printStackTrace();
                         }
 
@@ -294,15 +281,12 @@ public class OCRAnimationFragment extends Fragment {
         }
 
         /**
-         * @author Derzu
-         *
-         * @see http://stackoverflow.com/a/9194259/833622
-         *
-         * Converts a immutable bitmap to a mutable bitmap. This operation doesn't allocates
-         * more memory that there is already allocated.
-         *
          * @param imgIn - Source image. It will be released, and should not be used more
          * @return a copy of imgIn, but muttable.
+         * @author Derzu
+         * <p/>
+         * Converts a immutable bitmap to a mutable bitmap. This operation doesn't allocates
+         * more memory that there is already allocated.
          */
         public Bitmap convertToMutable(Bitmap imgIn) {
             try {
@@ -323,7 +307,7 @@ public class OCRAnimationFragment extends Fragment {
                 //Copy the byte to the file
                 //Assume source bitmap loaded using options.inPreferredConfig = Config.ARGB_8888;
                 FileChannel channel = randomAccessFile.getChannel();
-                MappedByteBuffer map = channel.map(FileChannel.MapMode.READ_WRITE, 0, imgIn.getRowBytes()*height);
+                MappedByteBuffer map = channel.map(FileChannel.MapMode.READ_WRITE, 0, imgIn.getRowBytes() * height);
                 imgIn.copyPixelsToBuffer(map);
                 //recycle the source bitmap, this will be no longer used.
                 imgIn.recycle();
